1938a75137bd8de169dcf585fa52f4f90c8dd742
cchs-react <jeonghun.seo@outlook.kr>
2024-09-20 15:06:45 +0900

    add Demo

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -14,7 +14,8 @@ export default function ProjectItem({data}) {
     const description = data.properties?.설명.rich_text[0].plain_text;
     const github = data.properties?.Github.url;
     const imgSrc = data.cover.file?.url || data.cover.external.url;
-    
+    const demo = data.properties?.Demo.url;
+    console.log(demo);
     const router = useRouter();
 
     function countWorkDays(start,end) {
@@ -33,30 +34,30 @@ export default function ProjectItem({data}) {
     
     const workDay = countWorkDays(start,end)
     return (
-            <Card sx={{width: 345}}>
-                <CardMedia
-                    sx={{height : 300}} 
-                    image={imgSrc}
-                    title={title}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {title}
-                    </Typography>
-                    <Typography variant="body1" sx={{color : 'text.secondary'}}>
-                        {workDay} 일
-                    </Typography>
-                    <Typography variant="body2" sx={{color : 'text.secondary'}}>
-                        {description}
-                    </Typography>
-                    <Typography variant="body3" sx={{color : 'text.secondary'}}>
-                        {tags.map((aTag) => (<span key={aTag.id}>#{aTag.name} </span>))}
-                    </Typography>
-
-                </CardContent>
-                <CardActions>
-                    <Button size="small" onClick={()=>{router.push(github)}} >Visit Github</Button>
-                </CardActions>
-            </Card>
+      <Card sx={{ width: 345, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+      <CardMedia sx={{ height: 300 }} image={imgSrc} title={title} />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+          {workDay} 일
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {description}
+        </Typography>
+        <Typography variant="body3" sx={{ color: 'text.secondary' }}>
+          {tags.map((aTag) => (
+            <span key={aTag.id}>#{aTag.name} </span>
+          ))}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={() => { router.push(github); }}>Visit Github</Button>
+        {demo && (
+          <Button size="small" onClick={() => { router.push(demo); }}>View Demo</Button>
+        )}
+      </CardActions>
+    </Card>
     )
 }
